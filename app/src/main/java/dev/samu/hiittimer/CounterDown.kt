cfba1d3c670db4308eb0d9840f118e45845b85fa
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-10-24 10:11:52 +0100

    Añadido GetReady

--- app/src/main/java/dev/samu/hiittimer/CounterDown.kt
+++ app/src/main/java/dev/samu/hiittimer/CounterDown.kt
@@ -1,13 +1,13 @@
 package dev.samu.hiittimer
 
+import android.media.MediaPlayer
 import android.os.CountDownTimer
 import android.util.Log
 
-class CounterDown(var segundos: Int, var loquehacealhacertick: (String) -> Unit) {
+class CounterDown(var segundos: Int, pitido: Boolean = false, val sonido: MediaPlayer? = null, var loquehacealhacertick: (String) -> Unit) {
     private var counterState: Boolean = false  // Indica si el temporizador está corriendo
     private var remainingTime: Long = ((segundos + 1) * 1000L)  // Tiempo restante en milisegundos
-    var isStarted: Boolean = false  // Indica si el temporizador ya ha empezado
-    var isPaused: Boolean = false  // Indica si está pausado
+    val pitido = pitido
 
     private var myCounter: CountDownTimer? = null
 
@@ -20,31 +20,27 @@ class CounterDown(var segundos: Int, var loquehacealhacertick: (String) -> Unit)
         myCounter = object : CountDownTimer(timeInMillis, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 remainingTime = millisUntilFinished
-                if (counterState) {
-                    loquehacealhacertick(formatTime(millisUntilFinished / 1000))
+                if (pitido){
+                    Log.i("errorr", "Entra")
+                    sonido?.start()
                 }
+                loquehacealhacertick(formatTime(millisUntilFinished / 1000))
             }
 
             override fun onFinish() {
                 counterState = false
-                isStarted = false
-                remainingTime = 0
-                loquehacealhacertick(formatTime(0))  // Mostrar 00:00 cuando termine
-                Log.i("dam2", "Timer finished")
+                loquehacealhacertick(formatTime(0))
             }
         }
     }
 
     // Alternar entre iniciar, pausar y reanudar
     fun toggle() {
-        Log.i("dam2", "toggle: $counterState")
         if (counterState) {
             pause()
         } else {
-            if (!isStarted) {
+            if (remainingTime > 0) {
                 start()
-            } else {
-                resume()
             }
         }
     }
@@ -52,8 +48,6 @@ class CounterDown(var segundos: Int, var loquehacealhacertick: (String) -> Unit)
     // Iniciar el temporizador desde el principio
     fun start() {
         counterState = true
-        isStarted = true
-        isPaused = false
         createTimer(remainingTime)  // Crea un temporizador nuevo desde el tiempo restante
         myCounter?.start()
     }
@@ -61,20 +55,7 @@ class CounterDown(var segundos: Int, var loquehacealhacertick: (String) -> Unit)
     // Pausar el temporizador
     fun pause() {
         counterState = false
-        myCounter?.cancel()  // Cancelamos el temporizador, pero mantenemos remainingTime
-        isPaused = true
-        Log.i("dam2", "Paused at: ${formatTime(remainingTime / 1000)}")
-    }
-
-    // Reanudar el temporizador desde el tiempo restante
-    fun resume() {
-        if (isPaused) {
-            counterState = true
-            createTimer(remainingTime)  // Recreamos el temporizador con el tiempo restante
-            myCounter?.start()
-            Log.i("dam2", "Resumed at: ${formatTime(remainingTime / 1000)}")
-            isPaused = false
-        }
+        myCounter?.cancel()
     }
 
     // Función para formatear el tiempo a "MM:SS"
